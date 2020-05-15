446d6fcc29eefa5285f81402e6d8144ed2439aa4
Michael Bade <mbade314@gmail.com>
2020-05-15 15:56:18 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -29,18 +29,29 @@ class String
   end
 
   def count_sentences
-    result = 0
-    if self.end_with?(".") || self.end_with?("!") || self.end_with?("?")
-
-      splitted = self.split(/[.!?]/)
-      splitted.each do |x|
-        if splitted[x] == []
-          result - 1
-        end
+    counter = 0
+    splitted = self.split(/[.!?]/)
+    splitted.each do |x|
+      if splitted[x] == []
+        counter - 1
       end
-
-      result = splitted.count
     end
-    result
+    counter = splitted.count
   end
+  counter
+end
+  #   result = 0
+  #   if self.end_with?(".") || self.end_with?("!") || self.end_with?("?")
+  # 
+  #     splitted = self.split(/[.!?]/)
+  #     splitted.each do |x|
+  #       if splitted[x] == []
+  #         result - 1
+  #       end
+  #     end
+  # 
+  #     result = splitted.count
+  #   end
+  #   result
+  # end
 end
